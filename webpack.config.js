f475628fa1269e7586ca90f6226a5e99fe8aecab
AIGLE <damien_aigle@hotmail.com>
2020-05-26 17:10:01 +0200

    version06

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const htmlWebpackTemplate = require("html-webpack-template");
 
 module.exports = {
   mode: "production",
@@ -14,6 +16,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Recipes App",
       meta: { viewport: "width=device-width, initial-scale=1.0" },
+      inject: true,
+      template: htmlWebpackTemplate,
+      links: [
+        "https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap",
+      ],
     }),
   ],
 
